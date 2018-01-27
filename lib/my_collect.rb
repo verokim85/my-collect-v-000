0846bf804982fb2d5ba45b563df43f3b903e56de
Veronica Kim <vero.a.kim@gmail.com>
2018-01-27 11:55:51 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+def my_collect(array)
+  i = 0
+  collection= []
 
+  while i < array.length
+  collection << yield(array[i])
+  i += 1
+end
+  collection
+end
+#
+# array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+# my_collect(array) do |name|
+#   name.split(" ").first
+# end
 
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
